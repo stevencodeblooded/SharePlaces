b68c870c5eb1217b76fbb8211b707ab73c60a59c
stevencodeblooded <steven.ochieng432@gmail.com>
2023-10-18 00:30:04 +0300

    Refactored the places-routes.js, To  Continue from Tommorow

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -4,22 +4,12 @@ const router = express.Router()
 //CONTROLLERS
 const placeControllers = require('../controllers/place-controllers')
 
-
 //------------------------ROUTES----------------------------
 
-//READING PLACES BASED ON PLACE ID 
 router.get('/:pid', placeControllers.getPlacesById)
-
-//READING PLACES BASED ON USER ID/CREATOR
 router.get('/users/:uid', placeControllers.getPlacesByUserId)
-
-//CREATING PLACES
 router.post('/', placeControllers.createdPlace)
-
-//UPDATE PLACES 
 router.patch('/:pid', placeControllers.updatePlace)
-
-// DELETING PLACES BASED ON PLACE ID
 router.delete('/:pid', placeControllers.deletePlace)
 
 module.exports = router
